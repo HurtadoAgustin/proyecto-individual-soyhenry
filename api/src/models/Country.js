9730c1763b751c9c04a03f06b1fa187a57ce9e19
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-10-17 22:18:42 -0300

    API: sequalize models created

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,42 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const CONTINENTS = ['America', 'Europe', 'Africa', 'Asia', 'Oceania'];
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('Country', {
+    id: {
+      type: DataTypes.CHAR(3),
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.ENUM(CONTINENTS),
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      validate: { min: 0 },
+    },
+  },
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
 };
