e454decbbecf703c4d18eec138a56c7012b9e238
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-10-18 15:47:48 -0300

    API: commented lines deleted

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 const countries = require('./countries.js');
 const activities = require('./activities.js');
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
 router.use('/countries', countries);
-
-//router.use('/activities', activities);
+router.use('/activities', activities);
 
 module.exports = router;
