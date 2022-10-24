236c1efdb96764f44c2753a974c657ff8b260c20
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-10-24 18:30:13 -0300

    CLIENT: changed filters, and now can filter by name, and sort by population, alphabetical, ascending and descending

--- client/src/containers/CountryList.js
+++ client/src/containers/CountryList.js
@@ -1,20 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCountries, getCountriesByName } from '../redux/actions.js';
 import CountryCard from '../components/CountryCard.js';
 
 function CountryList() {
   const dispatch = useDispatch();
-  const countryList = useSelector(state => state.countries);
+  const countries = useSelector(state => state.countries);
   const filters = useSelector(state => state.filters);
+  const [countryList, setCountryList] = useState([]);
 
   useEffect(() => {
     dispatch(
       (!filters.text)
         ? getAllCountries()
-        : getCountriesByName(filters.text)
+        : getCountriesByName(filters.text)  
     );
   }, [dispatch, filters]);
+  
+  useEffect(() => {
+    let data = [...countries];
+    data = (!!filters.typeAlpha)
+      ? data.sort((a,b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1)
+      : data.sort((a,b) => a.population - b.population)
+    ;
+    if(!filters.sortAsc) data = data.reverse();
+    setCountryList(data);
+  },[countries, filters])
 
   return <>
     {countryList?.map(country => <CountryCard
--- client/src/containers/ListFilters.js
+++ client/src/containers/ListFilters.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { saveFilters } from '../redux/actions.js';
 
+const initialFilters = {
+  page: 0,
+  text: '',
+  sortAsc: false, // true: sort ascending, false: sort descending
+  typeAlpha: false, // true: alphabetical, false: population
+  continent: '',
+  activity: {},
+}
+
 function ListFilters() {
   const dispatch = useDispatch();
-
-  const [filters, setFilters] = useState({
-    page: 0,
-    text: '',
-    ascendent: false,
-  });
+  const [filters, setFilters] = useState(initialFilters);
 
   useEffect(() => {
     dispatch(saveFilters(filters));
@@ -28,8 +32,12 @@ function ListFilters() {
     {...filters, text: e.target.value}
   );
 
-  const onAscendentButtonHandler = () => setFilters(
-    {...filters, ascendent: !filters.ascendent}
+  const onSortButtonHandler = () => setFilters(
+    {...filters, sortAsc: !filters.sortAsc}
+  );
+  
+  const onTypeButtonHandler = () => setFilters(
+    {...filters, typeAlpha: !filters.typeAlpha}
   );
 
   return <>
@@ -37,8 +45,11 @@ function ListFilters() {
     <div>{filters.page}</div>
     <button onClick={addPageHandler}>+</button>
     <input type='text' value={filters.text} onChange={onTextChangeHandler}/>
-    <button onClick={onAscendentButtonHandler}>
-      {filters.ascendent ? 'Ascendent' : 'Descendent'}
+    <button onClick={onSortButtonHandler}>
+      {filters.sortAsc ? 'Sort Ascending' : 'Sort Descending'}
+    </button>
+    <button onClick={onTypeButtonHandler}>
+      {filters.typeAlpha ? 'Alphabetical' : 'Population'}
     </button>
   </>
 }
