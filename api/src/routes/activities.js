251d57b5e92f53b9f8bf49b8fb95da84dcc3d2a3
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-10-25 15:17:12 -0300

    API: fixed .post method of /activity

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,16 +1,24 @@
 const router = require('express').Router();
 const { StatusCodes } = require('http-status-codes');
-const { Activity } = require('../db.js');
+const { Activity, Country } = require('../db.js');
 
 router.post('/', async (req, res) => {
-  const { name, duration, difficulty, season } = req.body;
+  const { name, duration, difficulty, season, countries } = req.body;
   try {
-    if(!name || !duration || !difficulty || !season) {
+    if(!name || !duration || !difficulty || !season || countries.length <= 1) {
       throw {message: 'Required information is missing', status: StatusCodes.BAD_REQUEST};
     }
-    const newActivity = await Activity.create(req.body);
+    const data = {...req.body};
+    delete data.countries;
+    const countriesArr = [...req.body.countries];
+    
+    const newActivity = await Activity.create(data);
     if(!newActivity) throw {message: 'Model creation error', status: StatusCodes.INTERNAL_SERVER_ERROR};
     
+    const selectedCountries = await Country.findAll({where: {id: countriesArr}});
+    if(selectedCountries.length !== countriesArr.length) throw {message: 'Country sended does not exist', status: StatusCodes.BAD_REQUEST};
+    
+    await newActivity.addCountries(selectedCountries);
     res.status(StatusCodes.CREATED).send(newActivity);
 
   } catch (error) {
