20887ebcdeb399c8997eaa7b1a4da9f04a1127f9
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-10-27 06:53:01 -0300

    CLIENT: css done of <ListFilters.js>

--- client/src/containers/ListFilters/ListFilters.js
+++ client/src/containers/ListFilters/ListFilters.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveFilters } from '../../redux/actions.js';
 import { initialFilters } from '../../utils/initialObjects.js';
+import './ListFilters.css';
 
 function ListFilters() {
   const dispatch = useDispatch();
