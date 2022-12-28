31004e52d2431a5ee48ea3b1d73fa0016456c215
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-12-28 12:45:21 -0300

    CLIENT: delete filters in client, to put it on api

--- client/src/components/CountryList/CountryList.js
+++ client/src/components/CountryList/CountryList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllCountries, getCountriesByName, clearError } from '../../redux/actions.js';
+import { getCountries, clearError } from '../../redux/actions.js';
+import { filterQueryParser } from '../../utils/parsers.js';
 import CountryCard from '../CountryCard/CountryCard.js';
 import ErrorHandler from '../ErrorHandler/ErrorHandler.js';
 
@@ -13,25 +14,14 @@ function CountryList() {
 
   useEffect(() => {
     dispatch(clearError());
-    dispatch(
-      (!filters.text)
-      ? getAllCountries()
-        : getCountriesByName(filters.text)  
-    );
+    dispatch(getCountries(filterQueryParser(filters)));
   }, [dispatch, filters]);
   
   useEffect(() => {
     let data = [...countries];
     if(!data.length) return;
-
-    data = (!!filters.typeAlpha)
-      ? data.sort((a,b) => a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1)
-      : data.sort((a,b) => a.population - b.population)
-    ;
-    if(!filters.sortAsc) data = data.reverse();
-    if(!!filters.continent) data = data.filter(country => country.continent === filters.continent);
     
-    const COUNTRIES_PER_PAGE = (filters.page === 0) ? 9 : 10; 
+    const COUNTRIES_PER_PAGE = 9;
     const dataPerPage = [];
     for(let i = 0; i < COUNTRIES_PER_PAGE; i++){
       let el = (COUNTRIES_PER_PAGE * filters.page) + i;
@@ -42,17 +32,18 @@ function CountryList() {
     setCountryList(dataPerPage);
   },[countries, filters])
 
-  return <>{ ( !error )
-    ? countryList?.map(country =>
-    <CountryCard
-      key={country.id}
-      id={country.id}
-      name={country.name}
-      flag={country.flag}
-      continent={country.continent}
-    />)
-    : <ErrorHandler type='grey' message={error}/>
-  }
+  return <>
+    { ( !error )
+      ? countryList?.map(country =>
+      <CountryCard
+        key={country.id}
+        id={country.id}
+        name={country.name}
+        flag={country.flag}
+        continent={country.continent}
+      />)
+      : <ErrorHandler type='grey' message={error}/>
+    }
   </>
 }
 
