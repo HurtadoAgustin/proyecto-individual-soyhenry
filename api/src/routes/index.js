e85eb37308dbdfae593d95b5661aeacfbfb7cf8b
Agustin Hurtado <agustin.hurtado@request.com.ar>
2023-03-25 14:07:00 -0300

    a

--- api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const countries = require('./countries.js');
-const activities = require('./activities.js');
-
-router.use('/countries', countries);
-router.use('/activities', activities);
-
-module.exports = router;
--- client/src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const SEASONS = ['summer', 'autumn', 'winter', 'spring'];
